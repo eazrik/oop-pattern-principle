7e30c490b7a0b848c226d7f7eb8b55d05bc03a97
eazrik <eazrik@gmail.com>
2018-06-23 22:50:10 +0800

    now class having metod complete

--- Task.php
+++ Task.php
@@ -3,13 +3,20 @@
 class Task
 {
     public $description;
+    public $completed = false;
 
     public function __construct($description)
     {
         $this->description = $description;
     }
+    public function complete()
+    {
+        $this->completed = true;
+    }
 }
 
 $task = new Task('Go to the store');
 
-var_dump($task->description);
+$task->complete();
+
+var_dump($task->completed);

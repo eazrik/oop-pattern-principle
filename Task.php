80b9ba5ff29b80e6f249fc92044802493763fb00
eazrik <eazrik@gmail.com>
2018-06-23 22:43:49 +0800

    class constructor

--- Task.php
+++ Task.php
@@ -2,9 +2,14 @@
 
 class Task
 {
-    public $description = 'Go to the store';
+    public $description;
+
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
 }
 
-$task = new Task;
+$task = new Task('Go to the store');
 
 var_dump($task->description);
